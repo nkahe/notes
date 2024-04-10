de25a8cecbc89b22429968a25a615f575b56242d
Henri K <nkahe@posteo.net>
2024-04-10 15:09:25 +0300

    ESLint lisää sääntöjä

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,10 +13,17 @@ export default [
         'error',
         'unix'
       ],
-      'eqeqeq': 'error'
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error', 'always'
+      ],
+      'arrow-spacing': [
+        'error', { 'before': true, 'after': true }
+      ]
     }
   },
   { ignores: [ "dist/*" ] },
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
 ];
